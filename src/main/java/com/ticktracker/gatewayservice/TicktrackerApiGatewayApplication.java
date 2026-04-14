551e9734dac50c456d1caee72c92a05e0788fc38
Aritra Sarkar <aritrasarkaraec@gmail.com>
2026-04-14 18:26:35 +0530

    Intial Commit

---
+++ src/main/java/com/ticktracker/gatewayservice/TicktrackerApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.ticktracker.gatewayservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicktrackerApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicktrackerApiGatewayApplication.class, args);
+	}
+
+}
